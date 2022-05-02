ceff322a3399e2e21bba1687b5c4be603b136ac3
Javed Gangjee <jgangjee@gmail.com>
2022-05-01 21:26:25 -0400

    default cell loading

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -7,6 +7,7 @@ import {
 } from '@angular/fire/auth';
 import {UserService} from './user.service';
 import {AngularFireDatabase} from '@angular/fire/compat/database';
+import {Cell} from '../models/cell.model';
 import {Board} from '../models/board.model';
 
 @Injectable({
@@ -15,6 +16,305 @@ import {Board} from '../models/board.model';
 export class AuthService {
   db: AngularFireDatabase;
   defaultBoardData;
+  public defaultCells: Cell[] = [
+    {
+      key: 'home',
+      backgroundColor: 'blue',
+      displayText: 'HOME',
+      route: '/home/home',
+      speakable: false,
+    },
+    {
+      key: 'yes-no',
+      backgroundColor: 'green',
+      displayText: 'YES/NO',
+      route: '/home/yes-no',
+      speakable: false,
+    },
+    {
+      key: 'core-4',
+      backgroundColor: 'blue',
+      displayText: '4 BUTTON',
+      route: '/home/core-4',
+      speakable: false,
+    },
+    {
+      key: 'core-6',
+      backgroundColor: 'red',
+      displayText: '6 BUTTON',
+      route: '/home/core-6',
+      speakable: false,
+    },
+    {
+      key: 'core-8',
+      backgroundColor: 'orange',
+      displayText: '8 BUTTON',
+      route: '/home/core-8',
+      speakable: false,
+    },
+    {
+      key: 'core-36',
+      backgroundColor: 'purple',
+      displayText: 'CORE 36',
+      route: '/home/core-36',
+      speakable: false,
+    },
+    {
+      key: 'yes',
+      backgroundColor: 'green',
+      displayText: 'YES',
+    },
+    {
+      key: 'no',
+      backgroundColor: 'red',
+      displayText: 'NO',
+    },
+    {
+      key: 'maybe',
+      backgroundColor: 'orange',
+      displayText: 'MAYBE',
+    },
+    {
+      key: 'stop',
+      backgroundColor: 'red',
+      displayText: 'STOP',
+    },
+    {
+      key: 'sure',
+      backgroundColor: 'blue',
+      displayText: 'SURE',
+    },
+    {
+      key: 'ok',
+      backgroundColor: 'purple',
+      displayText: 'OK',
+    },
+    {
+      key: 'hello',
+      backgroundColor: 'green',
+      displayText: 'HELLO',
+    },
+    {
+      key: 'bye',
+      backgroundColor: 'orange',
+      displayText: 'BYE',
+    }, {
+      key: 'like',
+      backgroundColor: 'green',
+      displayText: 'LIKE',
+    },
+    {
+      key: 'want',
+      backgroundColor: 'green',
+      displayText: 'WANT',
+    },
+    {
+      key: 'get',
+      backgroundColor: 'green',
+      displayText: 'GET',
+    },
+    {
+      key: 'make',
+      backgroundColor: 'green',
+      displayText: 'MAKE',
+    },
+    {
+      key: 'good',
+      backgroundColor: 'green',
+      displayText: 'GOOD',
+    },
+    {
+      key: 'more',
+      backgroundColor: 'green',
+      displayText: 'MORE',
+    },
+    {
+      key: 'not',
+      backgroundColor: 'red',
+      displayText: 'NOT',
+    },
+    {
+      key: 'go',
+      backgroundColor: 'green',
+      displayText: 'GO',
+    },
+    {
+      key: 'look',
+      backgroundColor: 'green',
+      displayText: 'LOOK',
+    },
+    {
+      key: 'turn',
+      backgroundColor: 'green',
+      displayText: 'TURN',
+    },
+    {
+      key: 'help',
+      backgroundColor: 'green',
+      displayText: 'HELP',
+    },
+    {
+      key: 'different',
+      backgroundColor: 'green',
+      displayText: 'DIFFERENT',
+    },
+    {
+      key: 'I',
+      backgroundColor: 'purple',
+      displayText: 'I',
+    },
+    {
+      key: 'he',
+      backgroundColor: 'purple',
+      displayText: 'HE',
+    },
+    {
+      key: 'open',
+      backgroundColor: 'green',
+      displayText: 'OPEN',
+    },
+    {
+      key: 'do',
+      backgroundColor: 'green',
+      displayText: 'DO',
+    },
+    {
+      key: 'put',
+      backgroundColor: 'green',
+      displayText: 'PUT',
+    },
+    {
+      key: 'same',
+      backgroundColor: 'green',
+      displayText: 'SAME',
+    },
+    {
+      key: 'you',
+      backgroundColor: 'purple',
+      displayText: 'YOU',
+    },
+    {
+      key: 'she',
+      backgroundColor: 'purple',
+      displayText: 'SHE',
+    },
+    {
+      key: 'that',
+      backgroundColor: 'purple',
+      displayText: 'THAT',
+    },
+    {
+      key: 'up',
+      backgroundColor: 'blue',
+      displayText: 'UP',
+    },
+    {
+      key: 'all',
+      backgroundColor: 'green',
+      displayText: 'ALL',
+    },
+    {
+      key: 'some',
+      backgroundColor: 'green',
+      displayText: 'SOME',
+    },
+    {
+      key: 'it',
+      backgroundColor: 'purple',
+      displayText: 'IT',
+    },
+    {
+      key: 'here',
+      backgroundColor: 'green',
+      displayText: 'HERE',
+    },
+    {
+      key: 'in',
+      backgroundColor: 'blue',
+      displayText: 'IN',
+    },
+    {
+      key: 'on',
+      backgroundColor: 'blue',
+      displayText: 'ON',
+    },
+    {
+      key: 'can',
+      backgroundColor: 'green',
+      displayText: 'CAN',
+    },
+    {
+      key: 'finished',
+      backgroundColor: 'red',
+      displayText: 'FINISHED',
+    },
+    {
+      key: 'where',
+      backgroundColor: 'orange',
+      displayText: 'WHERE',
+    },
+    {
+      key: 'what',
+      backgroundColor: 'orange',
+      displayText: 'WHAT',
+    },
+    {
+      key: 'why',
+      backgroundColor: 'orange',
+      displayText: 'WHY',
+    },
+    {
+      key: 'who',
+      backgroundColor: 'orange',
+      displayText: 'WHO',
+    },
+    {
+      key: 'when',
+      backgroundColor: 'orange',
+      displayText: 'WHEN',
+    },
+  ];
+
+
+  public defaultBoards: Board[] = [
+    {
+      key: 'home',
+      difficulty: 1,
+      title: 'SYMPLE',
+      cellArray: 'yes-no,core-4,core-6,core-8,core-8,core-36',
+    },
+    {
+      key: 'yes-no',
+      difficulty: 1,
+      title: 'Yes/No Board',
+      cellArray: 'yes,no',
+    },
+    {
+      key: 'core-4',
+      difficulty: 2,
+      title: '4 Button Board',
+      cellArray: 'yes,no,maybe,stop',
+    },
+    {
+      key: 'core-6',
+      difficulty: 2,
+      title: '6 Button Board',
+      cellArray: 'yes,no,maybe,stop,hello,bye',
+    },
+    {
+      key: 'core-8',
+      difficulty: 2,
+      title: '8 Button Board',
+      cellArray: 'yes,no,maybe,stop,sure,ok,hello,bye',
+    },
+    {
+      key: 'core-36',
+      difficulty: 5,
+      title: 'Core 36 Board',
+      cellArray: 'like,want,get,make,good,more,not,go,look,turn,help,different,I,he,open,do,put,same,you,she,that,up,all,some,it,here,in,on,can,finished,where,what,why,who,when',
+    },
+  ];
+
 
   constructor(db: AngularFireDatabase, private auth: Auth, private userService: UserService) {
     this.db = db;
@@ -23,12 +323,24 @@ export class AuthService {
   async register({email, password}) {
     try {
       const userAuth = await createUserWithEmailAndPassword(this.auth, email, password);
-      const cells = this.db.database.ref().child(userAuth.user.uid+ '_cells');
-      await cells.child('yes-no').set({key: 'yes-no', backgroundColor: 'green', displayText: 'YES/NO', route:'/home/yes-no'});
-      await cells.child('yes').set({key: 'yes', backgroundColor: 'green', displayText: 'YES'});
-      await cells.child('no').set({key: 'no', backgroundColor: 'red', displayText: 'NO'});
-      const  boards = this.db.database.ref().child(userAuth.user.uid+ '_boards');
-      await boards.child('home').set({key: 'home', difficulty: 1, title: 'YES/NO', cellArray:'yes,no'});
+      this.defaultCells.forEach((defaultCell) => {
+        const cells = this.db.database.ref().child(userAuth.user.uid + '_cells');
+        cells.child(defaultCell.key).set({
+          key: defaultCell.key,
+          backgroundColor: defaultCell.backgroundColor,
+          displayText: defaultCell.displayText
+        });
+      });
+      const boards = this.db.database.ref().child(userAuth.user.uid + '_boards');
+      this.defaultBoards.forEach((defaultBoard) => {
+        const cells = this.db.database.ref().child(userAuth.user.uid + '_boards');
+        cells.child(defaultBoard.key).set({
+          key: defaultBoard.key,
+          difficulty: defaultBoard.difficulty,
+          title: defaultBoard.title,
+          cellArray: defaultBoard.cellArray
+        });
+      });
       return userAuth;
     } catch (e) {
       return null;
